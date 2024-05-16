f1d5cab35f9f925e9fea4db8330deef42b11c8e8
tszh4922 <tszh4922@gmail.com>
2024-05-16 15:10:33 +0800

    Update _worker.src.js

--- _worker.src.js
+++ _worker.src.js
@@ -1,7 +1,7 @@
 // src/worker.js
 import { connect } from "cloudflare:sockets";
 let password = 'auto';
-let proxyIP = '';
+let proxyIP = 'proxyip.sg.fxxk.dedyn.io';
 
 let addresses = [
 	//当sub为空时启用本地优选域名/优选IP，若不带端口号 TLS默认端口为443，#号后为备注别名
